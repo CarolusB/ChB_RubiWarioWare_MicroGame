bb44ce6134a9f8766cdc5d61e5fa8952d78b8825
Charles BENTEIN <c.bentein@rubika-edu.com>
2020-12-02 12:09:25 +0100

    A test scene + script creation

---
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioBrigantin/ACouteauxTires/ScriptACouteauxTires/ACouteauxTiré_TimeBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioBrigantin
+{
+    namespace CouteauxTires
+    {
+        public class ACouteauxTiré_TimeBehavior : TimedBehaviour
+        {
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+
+            }
+        }
+    }
+}
---
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioBrigantin/ACouteauxTires/ScriptACouteauxTires/CrosshairController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioBrigantin
+{
+	namespace CouteauxTires
+	{
+		public class CrosshairController : MonoBehaviour
+		{
+			#region Variables
+			
+			#endregion
+
+			// Start is called before the first frame update
+			void Start()
+			{
+				
+			}
+
+			// Update is called once per frame
+			void Update()
+			{
+				
+			}
+		}
+	}
+}
