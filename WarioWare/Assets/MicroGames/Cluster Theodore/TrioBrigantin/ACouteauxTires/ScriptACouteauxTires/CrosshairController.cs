bb217d53abb4abd6bbe588d420ef9ec22fd65969
Charles BENTEIN <c.bentein@rubika-edu.com>
2020-12-02 15:24:07 +0100

    Crosshair movement and locking

--- WarioWare/Assets/MicroGames/Cluster Theodore/TrioBrigantin/ACouteauxTires/ScriptACouteauxTires/CrosshairController.cs
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioBrigantin/ACouteauxTires/ScriptACouteauxTires/CrosshairController.cs
@@ -6,22 +6,82 @@ namespace TrioBrigantin
 {
 	namespace CouteauxTires
 	{
+		/// <summary>
+		/// CHB -- Crosshair movement and interactions
+		/// </summary>
 		public class CrosshairController : MonoBehaviour
 		{
 			#region Variables
-			
-			#endregion
+			[Range(0.1f, 500f)]
+			[SerializeField] float movementSpeed = 1;
 
-			// Start is called before the first frame update
-			void Start()
-			{
+			Rigidbody2D crosshairRb;
+
+			static public CrosshairController instance = null;
+
+			// movement direction variables
+			float horizontal = 0, vertical = 0;
+			Vector2 movementVector = Vector2.zero;
+
+            //target enemy
+            public Enemy targetEnemy;
+            #endregion
+            private void Awake()
+            {
+				if(instance == null)
+                {
+					instance = this;
+                }
+                else
+                {
+					Destroy(gameObject);
+                }
 				
 			}
-
-			// Update is called once per frame
-			void Update()
+            // Start is called before the first frame update
+            void Start()
 			{
-				
+				crosshairRb = GetComponent<Rigidbody2D>();
+			}
+
+            // Update is called once per frame
+            void FixedUpdate()
+			{
+				Move();
+				Lock();
+			}
+
+			void Move()
+            {
+				horizontal = Input.GetAxis("Left_Joystick_X");
+				vertical = -Input.GetAxis("Left_Joystick_Y");
+
+				if (horizontal < -0.15 || horizontal > 0.15 || vertical < -0.15 || vertical > 0.15)
+                {
+					movementVector = new Vector2(horizontal, vertical);
+				}
+                else
+                {
+					movementVector = Vector2.zero;
+                }
+
+				crosshairRb.velocity = movementVector.normalized * movementSpeed;
+			}
+
+			void Lock()
+            {
+                if (Input.GetButtonDown("A_Button"))
+                {
+					if(targetEnemy != null)
+                    {
+						targetEnemy.TakeLock();
+
+						//spawn yellow crosshair copy
+					}
+
+					//spawn crooshair grey copy
+					//ammo--;
+				}
 			}
 		}
 	}
---
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioBrigantin/ACouteauxTires/ScriptACouteauxTires/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioBrigantin
+{
+	namespace CouteauxTires
+	{
+		public class Enemy : MonoBehaviour
+		{
+			#region Variables
+			CircleCollider2D detectZone;
+
+			[Range(1,2)]
+			[SerializeField] int needLock = 1;
+
+			bool isKilled;
+			#endregion
+
+			// Start is called before the first frame update
+			void Start()
+			{
+				detectZone = GetComponent<CircleCollider2D>();
+			}
+
+            #region Trigger calls
+            private void OnTriggerEnter2D(Collider2D collision)
+            {
+                if(collision.gameObject == CrosshairController.instance.gameObject)
+                {
+					CrosshairController.instance.targetEnemy = this;
+                }
+            }
+
+            private void OnTriggerExit2D(Collider2D collision)
+            {
+				if (collision.gameObject == CrosshairController.instance.gameObject)
+				{
+					CrosshairController.instance.targetEnemy = null;
+				}
+			}
+            #endregion
+
+			public void TakeLock()
+            {
+				needLock--;
+
+				if(needLock == 0)
+                {
+					//Manager enemyKilled++;
+					detectZone.enabled = false;
+					isKilled = true;
+                }
+            }
+        }
+    }
+}
